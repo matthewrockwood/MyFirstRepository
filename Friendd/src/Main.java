5e9706160d5ecb6aa3673b914ce553936c61e6e6
Matthew Rockwood <rockwoodmatthew@gmail.com>
2023-09-22 15:20:28 -0400

    myfirstpush

---
+++ Friendd/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		int num = 5;
+		float mynum = 4.44f;
+		boolean uk = false;
+		String ssd = "food";
+		
+		
+		
+		Scanner scanner= new Scanner(System.in); 
+		
+		System.out.println("What is your name?");
+		String name=scanner.nextLine();
+
+		System.out.println(name);
+	}
+
+}
